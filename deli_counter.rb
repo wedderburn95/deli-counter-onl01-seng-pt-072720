8ba69066b3dc01708dca702b5d76db7585c11bfc
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-04 21:22:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,4 +2,24 @@ def line(deli)
   if deli.empty?
     puts "The line is currently empty"
   else
-    current
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+  end
+  puts current_line
+  end
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def  now_serving(deli)
+  if deli.empty?
+    puts "there is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
