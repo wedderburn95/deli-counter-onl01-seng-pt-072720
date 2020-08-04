f87bceeb9dec6c8b05128d5b49c7540b85f130a0
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-04 21:23:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(deli)
   if deli.empty?
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
