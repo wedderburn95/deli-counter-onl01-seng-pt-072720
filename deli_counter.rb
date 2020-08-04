c9d398f40004446adefccbe90b23532a6288ba11
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-04 21:25:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(deli, name)
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
-def  now_serving(deli)
+def now_serving(deli)
   if deli.empty?
     puts "There is nobody waiting to be served!"
   else
