14575bd59d25ee092fed45da21abacaa06689c1b
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-04 21:24:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def  now_serving(deli)
   if deli.empty?
-    puts "there is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{deli.first}."
     deli.shift
